2a070ece9f92d01b982535b9af1a0cf1c12f7e5b
Nalla Sowmya <nalla.sowmya@testingxperts.loc>
2019-06-14 18:22:18 +0530

    test massage

---
+++ Test Suites/gitdemotestsuite1.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>gitdemotestsuite1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>720c7d96-0546-41aa-bc59-c0d3861eafbe</testSuiteGuid>
+</TestSuiteEntity>
